ae738c7c386001c333fc2784983189abfede697d
Sangam998416 <vivekpandey8957165770@gmail.com>
2024-10-14 16:48:25 +0000

    firstprogram in github

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "vivekpandey@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+// AccountId = 2 //not found
+
+accountEmail = "hv@gmail.com"
+accountPassword = "321321"
+accountCity = "rome"
+console.table([accountPassword,accountEmail,accountId,accountCity])
+console.log(accountId);
